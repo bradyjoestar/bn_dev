3bdfa9adb943edb7602a7f0f7d33e8c58cbde1d0
sha3n <qingruishen@yahoo.com>
2022-08-17 19:36:31 +0800

    [fix]: fix script

--- quick_build_and_start.sh
+++ quick_build_and_start.sh
@@ -9,7 +9,7 @@ source ./components/utils.sh
 #L1_CONTRACTS_OWNER_KEY="ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 #L2_CONTRACTS_OWNER_KEY="ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
-read -p "Enter full path to optimism: " PROJECT
+read -p "Enter full path to project: " PROJECT
 read -p "Enter L1 Network(local / georli): " L1_NETWORK
 read -p "Enter your contract deployer owner private-key: " CONTRACTS_OWNER_KEY
 read -p "Enter your contract deployer owner address: " CONTRACTS_OWNER_ADDRESS
@@ -19,11 +19,17 @@ read -p "Enter your contract deployer owner address: " CONTRACTS_OWNER_ADDRESS
 # do the real deploy
 cd ./components || return
 
+echo "\nbuild l1..."
 BUILD=true ./1_l1mock.sh $PROJECT
+echo "\nbuild deployer..."
 BUILD=true ./2_deployer.sh $PROJECT $L1_NETWORK $CONTRACTS_OWNER_KEY $CONTRACTS_OWNER_ADDRESS
+echo "\nbuild dtl..."
 BUILD=true ./3_dtl.sh $PROJECT
+echo "\nbuild l2geth..."
 BUILD=true ./4_l2geth.sh $PROJECT
+echo "\nbuild gasoracle..."
 BUILD=true ./5_gasoracle.sh $PROJECT
+echo "\nbuild batch_submitter..."
 BUILD=true ./6_batch_submitter.sh $PROJECT
 
 cd - || return
